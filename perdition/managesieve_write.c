d51aa7ed4ea1894de2d04fc45b57eb3967347422
Simon Horman <horms@verge.net.au>
2010-04-29 00:11:19 +1000

    managesieve: Add managesieve_write()
    This forms the basis of writing managesieve output.
    
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- perdition/managesieve_write.c
+++ perdition/managesieve_write.c
@@ -1,29 +1,143 @@
-#include "io.h"
-#include "token.h"
+#include "acap.h"
+#include "managesieve_response_code.h"
 #include "managesieve_write.h"
-#include "unused.h"
+#include "str.h"
+#include "io.h"
+#include "perdition_types.h"
+
+#include <stdarg.h>
+#include <vanessa_logger.h>
 
 /**********************************************************************
- * managesieve_write
- * Display an message of the form
- *	 <command>[ <string>]
- * or
- *	 <string>
- * Pre: io: io_t to write to
- *	flag: flag to pass to str_write as per str.h
- *	tag: ignored
- *	command: command in message sent
- *		 if NULL then only string is written
- *	nargs: number of arguments after fmt
- *	fmt: format passed used to form string
- *	...: arguments for fmt
- * Return 0 on success
- *	  -1 otherwise
- **********************************************************************/
+  * managesieve_write_raw
+  *       message: must not be NULL
+  *       message: must not be NULL
+  * Display an message without any alteration
+  **********************************************************************/
 
-int managesieve_write(io_t *UNUSED(io), flag_t UNUSED(flag),
-		      const token_t *UNUSED(tag), const char *UNUSED(command),
-		      size_t UNUSED(nargs), const char *UNUSED(fmt), ...)
+int managesieve_write_raw(io_t *io, const char *message)
 {
-	return -1;
+	if (str_write(io, 0, 1, "%s", message) < 0) {
+		VANESSA_LOGGER_DEBUG("str_write");
+		return -1;
+	}
+	return 0;
+}
+
+static int managesieve_write_list(io_t *io, size_t n, ...)
+{
+	char *out = NULL;
+	const char *str;
+	size_t i, x, len = 0;
+	int status = -1;
+	va_list ap, aq;
+
+	if (!n) {
+		VANESSA_LOGGER_DEBUG("No strings");
+		goto err;
+	}
+
+	va_start(ap, n);
+	va_copy(aq, ap);
+	for (i = 0; i < n; i++) {
+		str = va_arg(ap, const char *);
+		if (!str)
+			continue;
+		len += strlen(str) + 1;
+	}
+	va_end(ap);
+
+	if (!len) {
+		VANESSA_LOGGER_DEBUG("No non-NULL strings");
+		goto err;
+	}
+
+	out = malloc(len);
+	if (!out) {
+		VANESSA_LOGGER_DEBUG_ERRNO("malloc");
+		goto err;
+	}
+
+	*out = '\0';
+
+	x = 0;
+	va_start(aq, n);
+	for (i = 0; i < n; i++) {
+		str = va_arg(aq, const char *);
+		if (!str)
+			continue;
+		if (x)
+			strcat(out, " ");
+		strcat(out, str);
+		x++;
+	}
+	va_end(aq);
+
+	if (managesieve_write_raw(io, out) < 0) {
+		VANESSA_LOGGER_DEBUG("managesieve_write_raw");
+		goto err;
+	}
+
+	status = 0;
+err:
+	free(out);
+	return status;
+}
+
+int managesieve_write(io_t *io, flag_t flag, const char *command,
+		      const struct managesieve_response_code *rc,
+		      const char *message)
+{
+	char **rc_str = NULL, **rc_str_p, *tail;
+	const char *cmd;
+	STRUCT_ACAP(msg_acap);
+	int status = -1;
+
+	if (message) {
+		msg_acap = acap_from_str(message, flag);
+		if (acap_is_zero(&msg_acap)) {
+			VANESSA_LOGGER_DEBUG("acap_from_str");
+			return -1;
+		}
+	}
+
+	if (!rc) {
+		if (managesieve_write_list(io, 2, command, msg_acap.a) < 0) {
+			VANESSA_LOGGER_DEBUG("managesieve_write_list no rc");
+			goto err;
+		}
+	} else {
+		rc_str = managesieve_response_str(rc, flag);
+		if (!rc_str) {
+			VANESSA_LOGGER_DEBUG("managesieve_response_str");
+			goto err;
+		}
+
+		cmd = command;
+		tail = NULL;
+		for (rc_str_p = rc_str + 1; *rc_str_p; rc_str_p++) {
+			if (!*(rc_str_p + 1))
+				tail = msg_acap.a;
+			if (managesieve_write_list(io, 3, cmd,
+						  *rc_str_p, tail) < 0) {
+				VANESSA_LOGGER_DEBUG("managesieve_write_list "
+						    "rc");
+				goto err;
+			}
+			cmd = NULL;
+		}
+	}
+
+	if (msg_acap.b) {
+		if (managesieve_write_raw(io, msg_acap.b) < 0) {
+			VANESSA_LOGGER_DEBUG("str_vwrite no rc b");
+			goto err;
+		}
+	}
+
+	status = 0;
+err:
+	acap_free(&msg_acap);
+	managesieve_response_str_free(rc_str);
+	return status;
 }
--- perdition/managesieve_write.h
+++ perdition/managesieve_write.h
@@ -2,6 +2,8 @@
 #define PERDIITON_MANAGESIEVE_WRITE_H
 
 #include "io.h"
+#include "managesieve_response_code.h"
+#include "perdition_types.h"
 #include "token.h"
 #include "unused.h"
 
@@ -14,32 +16,53 @@
 #define MANAGESIEVE_DEFAULT_PORT "2000"
 
 /**********************************************************************
- * managesieve_write
- * Display an message of the form
- *	 <command>[ <string>]
- * or
- *	 <string>
- * Pre: io: io_t to write to
- *	flag: flag to pass to str_write as per str.h
- *	tag: ignored
- *	command: command in message sent
- *		 if NULL then only string is written
- *	nargs: number of arguments after fmt
- *	fmt: format passed used to form string
- *	...: arguments for fmt
- * Return 0 on success
- *	  -1 otherwise
- **********************************************************************/
+  * managesieve_write_raw
+  *       message: must not be NULL
+  *       message: must not be NULL
+  * Display an message without any alteration
+  **********************************************************************/
 
-int managesieve_write(io_t *io, flag_t flag, const token_t *tag,
-		      const char *command, size_t nargs, const char *fmt, ...);
+int managesieve_write_raw(io_t *io, const char *message);
+
+/**********************************************************************
+  * managesieve_write
+  * pre: io: io_t to write to
+  *       flag: PERDITION_CLIENT or PERDITION_SERVER
+  *       command: must not be NULL
+  *       rc: response code
+  *       message: must not be NULL
+  * Display an message of the form <command> [<rc>] <message>
+  **********************************************************************/
+
+int managesieve_write(io_t *io, flag_t flag, const char *command,
+		      const struct managesieve_response_code *rc,
+		      const char *message);
+
+/**********************************************************************
+ * managesieve_write_str
+ * Display an message of the form <command> [<string>]
+ * Pre: io: io_t to write to
+ *      flag: flag to pass to str_write as per str.h
+ *      tag: ignored
+ *      command: command in message sent
+ *           if NULL then only string is written
+ *      string: string, omitted if NULL
+ *           At least one of command and string must be non-NULL
+ * Return 0 on success
+ *        -1 otherwise
+ **********************************************************************/
 
 static inline int
 managesieve_write_str(io_t *io, const flag_t flag, const token_t *UNUSED(tag),
-	       const char *command, const char *str) {
-	if (str)
-		return managesieve_write(io, flag, NULL, command, 1, "%s", str);
-	return managesieve_write(io, flag, NULL, command, 0, NULL);
+		      const char *command, const char *str)
+{
+	if (!command && !str)
+		return -1;
+	if (!command)
+		/* This is a special case used to handle opt.server_resp_line,
+		 * and jsut requires str to be written verbatim. */
+		 return str_write(io, WRITE_STR_NO_CLLF, 1, "%s", str);
+	return managesieve_write(io, flag, command, NULL, str);
 }
 
 #endif /* PERDIITON_MANAGESIEVE_WRITE_H */
