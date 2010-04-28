18ec6fe995c06124c605454e40f7f094f9c343ef
Simon Horman <horms@verge.net.au>
2010-04-29 00:11:19 +1000

    core: auth structure
    Up until now perdition has only handled logins using simple user name
    and password pairs. IMAP's LOGIN command, and the POP's USER and PASS
    commands. Managesieve has no such commands and uses SASL in conjunction
    with its AUTHENTICATE command.
    
    The purpose of this patch is to provide an abstraction for
    user/password tuples and SASL.
    
    This should also be useful for IMAP which also has SASL in conjunction
    with an AUTHENTICATE command.
    
    Signed-off-by: Simon Horman <horms@verge.net.au>

---
+++ perdition/auth.h
@@ -0,0 +1,71 @@
+#ifndef PERDITION_AUTH_H
+#define PERDITION_AUTH_H
+
+#include <string.h>
+#include <stdlib.h>
+
+struct auth {
+	char *authorisation_id; /* Optional user to use the credentials of
+				 * to access authentication_id's account.
+				 * E.g. admin accessing a user's account */
+	char *authentication_id;
+	char *passwd;
+
+	enum {
+		auth_t_none = 0,
+		auth_t_passwd
+	} type;
+};
+
+#define STRUCT_AUTH(name) \
+	struct auth (name) = { .type = auth_t_none }
+
+static inline void auth_zero(struct auth *auth)
+{
+	memset(auth, 0, sizeof(auth));
+}
+
+static inline void auth_set_authorisation_id(struct auth *auth, char *id)
+{
+	if (auth->authorisation_id)
+		auth->authorisation_id = id;
+	else
+		auth->authentication_id = id;
+}
+
+static inline char *auth_get_authorisation_id(const struct auth *auth)
+{
+	if (auth->authorisation_id)
+		return auth->authorisation_id;
+	return auth->authentication_id;
+}
+
+static inline void auth_free_data(const struct auth *auth)
+{
+	if (!auth)
+		return;
+
+	switch (auth->type) {
+	case auth_t_none:
+		return;
+	case auth_t_passwd:
+		break;
+	}
+
+	free(auth->authorisation_id);
+	free(auth->authentication_id);
+	free(auth->passwd);
+}
+
+static inline struct auth auth_set_pwd(char *name, char *passwd)
+{
+	struct auth a = {
+		.type = auth_t_passwd,
+		.authentication_id = name,
+		.passwd = passwd
+	};
+
+	return a;
+}
+
+#endif /* PERDITION_AUTH_H */
